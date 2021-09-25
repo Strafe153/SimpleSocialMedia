0e375b3a97b0d1518a5602ac020c827275327c06
Vitalii Knysh <knish.vital@gmail.com>
2021-09-25 03:37:35 +0300

    Changed Account/Index parameters and Account/Edit redirect on "Save"

---
+++ IdentityApp/ViewModels/AdminPanelViewModel.cs
@@ -0,0 +1,14 @@
+﻿using IdentityApp.Models;
+
+namespace IdentityApp.ViewModels
+{
+    public class AdminPanelViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public int? Year { get; set; }
+        public string Country { get; set; }
+        public int Page { get; set; } = 1;
+        public SortState SortOrder { get; set; } = SortState.NameAscending;
+    }
+}
