5ccf0a09d5ce8e754fb3bb345658356d7471cbec
Vitalii Knysh <knish.vital@gmail.com>
2021-10-03 23:48:06 +0300

    Limited the length of a post while creating and editing to 0-350 symbols

--- IdentityApp/ViewModels/EditPostViewModel.cs
+++ IdentityApp/ViewModels/EditPostViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using IdentityApp.Models;
 
 namespace IdentityApp.ViewModels
@@ -8,7 +9,10 @@ namespace IdentityApp.ViewModels
     public class EditPostViewModel
     {
         public string Id { get; set; }
+
+        [MaxLength(350)]
         public string Content { get; set; }
+
         public string UserId { get; set; }
         public string UserName { get; set; }
         public DateTime PostedTime { get; set; }
