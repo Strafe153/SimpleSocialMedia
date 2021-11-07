f6f62da2c2943cc912d9c65192f97e27e75090f1
Vitalik Knysh <knish.vital@gmail.com>
2021-11-07 03:39:03 +0200

    Add home page post comments deletion and editing

--- IdentityApp/Controllers/PostCommentController.cs
+++ IdentityApp/Controllers/PostCommentController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using IdentityApp.Models;
+using IdentityApp.ViewModels;
 using IdentityApp.Interfaces;
 
 namespace IdentityApp.Controllers
@@ -15,11 +20,13 @@ namespace IdentityApp.Controllers
             _repository = repository;
         }
 
-        public async Task<IActionResult> Create(string postId, string commentAuthorName, string postContent)
+        public async Task<IActionResult> Create(string postId, string commentAuthorName, 
+            string postContent, int page)
         {
             if (!string.IsNullOrEmpty(postId))
             {
-                Post post = await _repository.FirstOrDefaultAsync(postId);
+                Post post = await _repository.FirstOrDefaultAsync(
+                    _repository.GetAllPosts(), post => post.Id == postId);
 
                 if (post != null)
                 {
@@ -45,13 +52,88 @@ namespace IdentityApp.Controllers
                         await _repository.SaveChangesAsync();
                         _repository.LogInformation($"User {commentAuthorName} created a comment");
 
-                        return RedirectToAction("Index", "Home");
+                        return RedirectToAction("Index", "Home", new { page = page });
                     }
                 }
             }
 
             _repository.LogError("Post not found");
             return NotFound();
+        }
+
+        public async Task<IActionResult> Delete(string postCommentId, int page)
+        {
+            if (!string.IsNullOrEmpty(postCommentId))
+            {
+                PostComment comment = await _repository.FirstOrDefaultAsync(
+                    _repository.GetAllComments(), comment => comment.Id == postCommentId);
+
+                if (comment == null)
+                {
+                    _repository.LogError("Comment not found");
+                    return NotFound();
+                }
+
+                comment.Post.PostComments.Remove(comment);
+                await _repository.SaveChangesAsync();
+                _repository.LogInformation($"User {comment.Author}'s post was deleted");
+
+                return RedirectToAction("Index", "Home", new { page = page });
+            }
+
+            _repository.LogError("Comment id is not passed");
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(string postCommentId, string returnUrl, int page)
+        {
+            if (!string.IsNullOrEmpty(postCommentId))
+            {
+                PostComment comment = await _repository.FirstOrDefaultAsync(
+                    _repository.GetAllComments(), comment => comment.Id == postCommentId);
+
+                if (comment == null)
+                {
+                    _repository.LogError("Comment not found");
+                    return NotFound();
+                }
+
+                EditPostCommentViewModel model = new EditPostCommentViewModel()
+                {
+                    CommentId = postCommentId,
+                    Content = comment.Content,
+                    Author = comment.Author,
+                    Page = page,
+                    ReturnUrl = returnUrl
+                };
+
+                return View(model);
+            }
+
+            _repository.LogError("Comment id is not passed");
+            return BadRequest();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditPostCommentViewModel model)
+        {
+            PostComment comment = await _repository.FirstOrDefaultAsync(
+                _repository.GetAllComments(), comment => comment.Id == model.CommentId);
+
+            if (comment == null)
+            {
+                _repository.LogError("Comment not found");
+                return NotFound();
+            }
+
+            comment.Content = model.Content;
+            comment.IsEdited = true;
+            _repository.GetAllComments().Update(comment);
+            await _repository.SaveChangesAsync();
+            _repository.LogInformation($"User {model.Author}'s post was edited");
+
+            return RedirectToAction("Index", "Home", new { page = model.Page });
         }
     }
 }
